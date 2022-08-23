03e93513c9866212e1291887f39efe221e107e81
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-08-23 14:54:55 -0500

    conversion and coercion

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
+// Type Conversion and coercion
+const inputYear = '1991.50'
+console.log(Number(inputYear) + 8)
 
+//type coercion
+console.log('23' + '10' + 3) //prints 23103. Addition contactinates
+console.log('23' - '10' - 3) //prints 10. Subtraction subtracts
 
+console.log('23' * '2') //prints 46
+
+let n = '1' + 1
+n = n -1
+console.log(n) //prints 10
+
+console.log('2'+2+3+4+'5') //prints 22345
+console.log(2+3+4+'5') //prints 95
